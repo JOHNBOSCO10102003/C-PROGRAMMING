0e14874f37430392ff004e4c586ded96cf9c6c49
JOHN BOSCO A <123642629+JOHNBOSCO10102003@users.noreply.github.com>
2023-01-29 14:22:34 +0530

    Create Pattern-1.C

---
+++ PATTERNS/Pattern-1.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("\nEnter no of Rows : ");
+	scanf("%d",&n);
+	for(i=0; i<n; i++)
+	{
+		for(j=0; j<n; j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+/* 
+
+* * * * *
+* * * * *
+* * * * *
+* * * * * 
+* * * * *
+
+*/
